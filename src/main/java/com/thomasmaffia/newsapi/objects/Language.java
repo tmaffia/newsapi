9c65fecc9638ceb2d92906335a1223aae898e9fd
tmaffia <ta.maffia@gmail.com>
2017-01-10 01:04:44 -0600

    add available languages to Enum

--- src/main/java/com/thomasmaffia/newsapi/objects/Language.java
+++ src/main/java/com/thomasmaffia/newsapi/objects/Language.java
@@ -4,4 +4,17 @@ package com.thomasmaffia.newsapi.objects;
  * Created by tmaffia on 1/10/17.
  */
 public enum Language {
+    ENGLISH("en"),
+    GERMAN("de"),
+    FRENCH("fr");
+
+    private String languageCode;
+
+    Language(final String languageCode) {
+        this.languageCode = languageCode;
+    }
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
 }
