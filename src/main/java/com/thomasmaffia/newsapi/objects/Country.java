204e17b91ff11fddf1f314e0a80b11acca727af4
tmaffia <ta.maffia@gmail.com>
2017-01-10 01:45:10 -0600

    add Countries to enum

--- src/main/java/com/thomasmaffia/newsapi/objects/Country.java
+++ src/main/java/com/thomasmaffia/newsapi/objects/Country.java
@@ -4,4 +4,20 @@ package com.thomasmaffia.newsapi.objects;
  * Created by tmaffia on 1/10/17.
  */
 public enum Country {
+    AUSTRALIA("au"),
+    GERMANY("de"),
+    UNITED_KINGDOM("gb"),
+    INDIA("in"),
+    ITALY("it"),
+    UNITED_STATES("us");
+
+    private final String countryCode;
+
+    Country(final String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
 }
